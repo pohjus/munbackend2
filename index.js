1025736a976ca2afca256273208b12fd58b63a61
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-12-03 09:36:30 +0200

    mysql

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 
 let port = process.env.PORT || 8080;
 
+let config2 = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
+
+var pool = mysql.createPool(config2);
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  pool.query("SELECT * from location", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 const server = app.listen(port, () => {
