63345fd066b8026dc4af97346fd19089647c92c7
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-12-03 09:29:16 +0200

    heroku

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+let port = process.env.PORT || 8080;
 
-const server = app.listen(8080, () => {
-  console.log(`Listening on port ${server.address().port}`)
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${server.address().port}`);
+});
